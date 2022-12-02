2af214a5356871a425dd1f1c4fcf22af1c1555d3
Kurision <kumalanil34@gmail.com>
2022-12-02 15:34:52 +0545

    Checking

--- src/dec2/FileDemo.java
+++ src/dec2/FileDemo.java
@@ -1,8 +1,8 @@
-
-package dec2;
-import java.io.*;
-public class FileDemo {
-    public void FileCopy(){
-        String Nfile = "/home/anil/Pictures/ei.png";
-    }
-}
+//not needed
+// package dec2;
+// import java.io.*;
+// public class FileDemo {
+//     public void FileCopy(){
+//         String Nfile = "/home/anil/Pictures/ei.png";
+//     }
+// }
